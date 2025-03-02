2b627ef32ebb7f019368a1a56a41d9f3b2520077
marcelooliveira <mclricardo@gmail.com>
2025-03-01 21:42:24 -0300

    4478-seguranca-aspnetcore-2-video1.3-Adicionando Controlador de Autenticação.mp4

---
+++ MedVoll.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using MedVoll.Web.Dtos;
+using MedVoll.WebAPI.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedVoll.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly UserManager<IdentityUser> userManager;
+    private readonly SignInManager<IdentityUser> signInManager;
+    public AuthController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    {
+        this.userManager = userManager;
+        this.signInManager = signInManager;
+    }
+
+    //Endpoints
+    [HttpPost("registrar-usuario")]
+    public async Task<IActionResult> RegistrarUsuarioAsync([FromBody] UsuarioDto usuarioDto)
+    {
+        var usuarioReg = await userManager.FindByEmailAsync(usuarioDto.Email!);
+        if (usuarioReg is not null)
+        {
+            return BadRequest("Usuário já foi registrado na base de dados.");
+        }
+
+        var usuario = new IdentityUser
+        {
+            UserName = usuarioDto.Email,
+            Email = usuarioDto.Email,
+            EmailConfirmed = true
+        };
+        var result = await userManager.CreateAsync(usuario, usuarioDto.Senha);
+        if (!result.Succeeded)
+        {
+            return BadRequest($"Falha ao registrar usuário : {result.Errors}");
+        }
+        await signInManager.SignInAsync(usuario, isPersistent: false);
+
+        return Ok("Usuário Criado com sucesso!");
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginAsync([FromBody] UsuarioDto usuarioDto)
+    {
+        var result = await signInManager.PasswordSignInAsync(usuarioDto.Email!, usuarioDto.Senha!, isPersistent: false, lockoutOnFailure: false);
+        if (!result.Succeeded)
+        {
+            return BadRequest("Falha no login do usuário.");
+        }
+        return Ok("Logado com sucesso!");
+    }
+
+}
